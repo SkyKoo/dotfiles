d15d76c7bad68dbcf603af59176d142d3868bdde
guchengming <guchengming@live.cn>
2026-09-10 21:11:55 +0800

    lazy and markview“

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,73 +1,76 @@
--- Install Packer automatically if it's not installed(Bootstraping)
--- Hint: string concatenation is done by `..`
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    -- install_path is ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local output = vim.fn.system({
+        'git', 'clone', '--filter=blob:none', '--branch=stable',
+        'https://github.com/folke/lazy.nvim.git', lazypath,
+    })
+    if vim.v.shell_error ~= 0 then
+        error('Failed to install lazy.nvim:\n' .. output)
     end
-    return false
 end
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
+require('lazy').setup({
+    { 'tanvirtin/monokai.nvim' },
+    {
+        'p00f/alabaster.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('colorscheme')
+        end,
+    },
 
--- Reload configurations if we modify plugins.lua
--- Hint
---     <afile> - replaced with the filename of the buffer being manipulated
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+    { 'williamboman/mason.nvim', lazy = false },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        lazy = false,
+        dependencies = {
+            'williamboman/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
+    },
+    { 'neovim/nvim-lspconfig', lazy = false },
 
+    {
+        'hrsh7th/nvim-cmp',
+        lazy = false,
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+        },
+        config = function()
+            require('config.nvim-cmp')
+        end,
+    },
 
--- Install plugins here - `use ...`
--- Packer.nvim hints
---     after = string or list,           -- Specifies plugins to load before this plugin. See "sequencing" below
---     config = string or function,      -- Specifies code to run after this plugin is loaded
---     requires = string or list,        -- Specifies plugin dependencies. See "dependencies".
---     ft = string or list,              -- Specifies filetypes which load this plugin.
---     run = string, function, or table, -- Specify operations to be run after successful installs/updates of a plugin
-return require('packer').startup(function(use)
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
+    {
+        'kyazdani42/nvim-tree.lua',
+        dependencies = { 'kyazdani42/nvim-web-devicons' },
+    },
 
-        ---------------------------------------
-        -- NOTE: PUT YOUR THIRD PLUGIN HERE --
-        ---------------------------------------
-        use 'tanvirtin/monokai.nvim'
-        use 'p00f/alabaster.nvim'
-
-        use { 'williamboman/mason.nvim' }
-        use { 'williamboman/mason-lspconfig.nvim'}
-
-        use { 'neovim/nvim-lspconfig' }
-        use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }
-        use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
-        use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' } -- buffer auto-completion
-        use { 'hrsh7th/cmp-path', after = 'nvim-cmp' } -- path auto-completion
-        use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' } -- cmdline auto-completion
-        use 'L3MON4D3/LuaSnip'
-        use 'saadparwaiz1/cmp_luasnip'
-
-        use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
-
-        -- install without yarn or npm
-        use({
-            "iamcco/markdown-preview.nvim",
-            run = function() vim.fn["mkdp#util#install"]() end,
-        })
-
-        use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-
-        -- Automatically set up your configuration after cloning packer.nvim
-        -- Put this at the end after all plugins
-        if packer_bootstrap then
-            require('packer').sync()
-        end
-    end)
-
+    {
+        'OXY2DEV/markview.nvim',
+        lazy = false,
+        opts = {
+            preview = {
+                filetypes = { 'markdown' },
+                modes = { 'n', 'no', 'c' },
+                hybrid_modes = {},
+            },
+        },
+        keys = {
+            { '<leader>v', '<cmd>Markview toggle<cr>', ft = 'markdown', desc = 'Toggle Markdown preview' },
+            { '<leader>V', '<cmd>Markview splitToggle<cr>', ft = 'markdown', desc = 'Toggle Markdown split preview' },
+        },
+    },
+}, {
+    -- Preserve eager loading where existing configuration requires plugin modules.
+    defaults = { lazy = false, version = false },
+    checker = { enabled = false },
+    rocks = { enabled = false },
+})
